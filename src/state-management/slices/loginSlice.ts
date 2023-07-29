09531fa473f8ba23959896f59a88fc8eafda2f93
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-07-29 07:13:14 +0300

    DONE: setup the login slice

---
+++ src/state-management/slices/loginSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../store'
+
+// Define a type for the slice state
+interface LoginState {
+  isLoggedIn: boolean
+}
+
+// Define the initial state using that type
+const initialState: LoginState = {
+  isLoggedIn: false
+}
+
+export const logInSlice = createSlice({
+  name: 'login',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    setIsLoggedIn: (state, action: PayloadAction<boolean>) => {
+      state.isLoggedIn = action.payload
+    }
+  }
+})
+
+export const { setIsLoggedIn } = logInSlice.actions
+
+// Other code such as selectors can use the imported `RootState` type
+export const isLoggedInSelector = (state: RootState) => state.isLoggedIn
+
+export default logInSlice.reducer
