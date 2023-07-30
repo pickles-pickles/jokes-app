6e56d531e6cba63da190a7617cdc2ace7e4bb98f
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-07-30 16:47:10 +0300

    DONE: go to home functionality

--- src/components/JokeEditor/JokesEditorForm.tsx
+++ src/components/JokeEditor/JokesEditorForm.tsx
@@ -1,9 +1,74 @@
+import { Container, Grid, TextField } from '@mui/material'
 import React from 'react'
+import { AppDispatch } from '../../store'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  jokeToEditSelector,
+  setJokeToEdit
+} from '../../state-management/slices/editJokeSlice.ts'
 
 const JokeEditorForm = () => {
+  const dispatch = useDispatch<AppDispatch>()
+  const jokeToEdit = useSelector(jokeToEditSelector)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    dispatch(setJokeToEdit({ ...jokeToEdit, [name]: value }))
+  }
   return (
     <>
       <h2>Hello from jokes editor form</h2>
+      <Container maxWidth='sm'>
+        <form>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label='Title'
+                name='title'
+                value={jokeToEdit.title}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label='Body'
+                name='body'
+                value={jokeToEdit.body}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label='Author'
+                name='author'
+                value={jokeToEdit.author}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label='Views'
+                name='views'
+                value={jokeToEdit.views}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label='Created at'
+                name='createdAt'
+                value={jokeToEdit.createdAt}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+        </form>
+      </Container>
     </>
   )
 }
--- src/pages/joke-editor-page/JokeEditorPage.tsx
+++ src/pages/joke-editor-page/JokeEditorPage.tsx
@@ -4,17 +4,40 @@ import { useSelector } from 'react-redux'
 import {
   deleteJokeThunk,
   jokeToEditSelector,
+  setJokeToEdit,
   updateJokeThunk
 } from '../../state-management/slices/editJokeSlice.ts'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../../store.ts'
+import { Link } from 'react-router-dom'
+import { Button, Container } from '@mui/material'
+import { styled } from '@mui/material/styles'
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  // hide last border in the row
+  marginRight: 5,
+  '&:last-child': {
+    marginRight: 0
+  }
+}))
+
+const StyledLink = styled(Link)(({ theme }) => ({
+  color: theme.palette.background.default
+}))
 
 const JokeEditorPage = () => {
   const dispatch = useDispatch<AppDispatch>()
   const jokeToEdit = useSelector(jokeToEditSelector)
+
   useEffect(() => {
     console.log('joke to edit', jokeToEdit)
   }, [jokeToEdit])
+
+  /* useEffect(() => {
+    return () => {
+      dispatch(setJokeToEdit({}))
+    }
+  }, []) */
 
   const submitJoke = () => {
       dispatch(updateJokeThunk(jokeToEdit))
@@ -26,8 +49,26 @@ const JokeEditorPage = () => {
     <>
       <h1>Hello from Joke editor page</h1>
       <JokeEditorForm />
-      <button onClick={submitJoke}>update joke</button>
-      <button onClick={submitDeleteJoke}>delete joke</button>
+      <Container maxWidth='sm'></Container>
+
+      <div style={{ marginTop: 10 }}>
+        <StyledButton variant='contained'>
+          <StyledLink to='/home' onClick={() => dispatch(setJokeToEdit({}))}>
+            Go home{' '}
+          </StyledLink>
+        </StyledButton>
+
+        <StyledButton variant='contained' color='success' onClick={submitJoke}>
+          update joke
+        </StyledButton>
+        <StyledButton
+          variant='contained'
+          color='secondary'
+          onClick={submitDeleteJoke}
+        >
+          delete joke
+        </StyledButton>
+      </div>
     </>
   )
 }
